5e4fa586ea8cfff712ece83aff4f8dc314151db9
Mahala444 <jmj42@usf.edu>
2025-06-05 14:33:05 -0500

    Added cc_3.js with initial customer records

---
+++ cc_3.js
@@ -0,0 +1,17 @@
+const customers = [
+  {
+    name: "Alice Jane",
+    email: "alice@hotmail.com",
+    purchases: ["Phone", "Charger"]
+  },
+  {
+    name: "Bobby Thomas",
+    email: "bobby@hotmail.com",
+    purchases: ["Laptop"]
+  },
+  {
+    name: "Carl Jones",
+    email: "carl@hotmail.com",
+    purchases: ["Tablet", "Case"]
+  }
+];
